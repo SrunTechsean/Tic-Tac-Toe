5eb83393b718910d517c466d15e08f5e8607ac85
SrunTechsean <techseansrun@gmail.com>
2026-06-06 10:09:12 +0700

    feat(script): add method to print board

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ function GameBoard() {
     const columns = 3;
     const board = [];
 
+    // Create a 2d array that will represent the state of the game board
+    // row 0 represent the top row and column 0 represent the left-most column.
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
@@ -10,6 +12,11 @@ function GameBoard() {
         }
     }
 
+    // Method to get our board that the UI will need to render
     const getBoard = () => board;
-    return { getBoard };
+
+    // Method to print out the board in the console
+    const printBoard = () => console.log(board);
+
+    return { getBoard, printBoard };
 }
