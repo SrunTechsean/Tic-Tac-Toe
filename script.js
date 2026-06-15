b65902de16fa519673888c124f812d8eaed353c9
SrunTechsean <techseansrun@gmail.com>
2026-06-16 00:08:23 +0700

    chore(script): change indent from 4 to 2

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 function GameBoard() {
-    const rows = 3;
-    const columns = 3;
-    const board = [];
+  const rows = 3;
+  const columns = 3;
+  const board = [];
 
-    // Create a 2d array that will represent the state of the game board
-    // row 0 represent the top row and column 0 represent the left-most column.
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push(Cell());
-        }
+  // Create a 2d array that will represent the state of the game board
+  // row 0 represent the top row and column 0 represent the left-most column.
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
     }
+  }
 
-    // Method to get our board that the UI will need to render
-    const getBoard = () => board;
+  // Method to get our board that the UI will need to render
+  const getBoard = () => board;
 
-    // Method to print out the board in the console
-    const printBoard = () => {
-        const boardWithCellValues = board.map((row) =>
-            row.map((cell) => cell.getValue()),
-        );
-        console.log(boardWithCellValues);
-    };
+  // Method to print out the board in the console
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue()),
+    );
+    console.log(boardWithCellValues);
+  };
 
-    const dropToken = (row, column, player) => {
-        const cell = board[row][column];
-        cell.addToken(player);
-    };
+  const dropToken = (row, column, player) => {
+    const cell = board[row][column];
+    cell.addToken(player);
+  };
 
-    return { getBoard, printBoard, dropToken };
+  return { getBoard, printBoard, dropToken };
 }
 
 /*
@@ -39,20 +39,20 @@ function GameBoard() {
  */
 
 function Cell() {
-    let value = 0;
+  let value = 0;
 
-    // Accept a player's token to change the value of the cell
-    const addToken = (player) => {
-        value = player;
-    };
+  // Accept a player's token to change the value of the cell
+  const addToken = (player) => {
+    value = player;
+  };
 
-    // How we will retrieve the current value of this cell through closure
-    const getValue = () => value;
+  // How we will retrieve the current value of this cell through closure
+  const getValue = () => value;
 
-    return {
-        addToken,
-        getValue,
-    };
+  return {
+    addToken,
+    getValue,
+  };
 }
 
 /*
@@ -62,240 +62,233 @@ function Cell() {
  */
 
 function GameController(
-    playerOneName = "Player One",
-    playerTwoName = "Player Two",
+  playerOneName = "Player One",
+  playerTwoName = "Player Two",
 ) {
-    let board = GameBoard();
-    const players = [
-        {
-            name: playerOneName,
-            token: "O",
-        },
-        {
-            name: playerTwoName,
-            token: "X",
-        },
-    ];
+  let board = GameBoard();
+  const players = [
+    {
+      name: playerOneName,
+      token: "O",
+    },
+    {
+      name: playerTwoName,
+      token: "X",
+    },
+  ];
 
-    // Make Player 1(player[0]) go first
-    let activePlayer = players[0];
-    let winner = null;
-    let gameOver = null;
+  // Make Player 1(player[0]) go first
+  let activePlayer = players[0];
+  let winner = null;
+  let gameOver = null;
 
-    const switchPlayerTurn = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    };
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
 
-    const getActivePlayer = () => activePlayer;
+  const getActivePlayer = () => activePlayer;
 
-    const printNewRound = () => {
-        board.printBoard();
-    };
+  const printNewRound = () => {
+    board.printBoard();
+  };
 
-    // Check For Winner
-    const checkWinner = () => {
-        const currentBoard = board.getBoard();
-        const playerToken = activePlayer.token;
+  // Check For Winner
+  const checkWinner = () => {
+    const currentBoard = board.getBoard();
+    const playerToken = activePlayer.token;
 
-        for (let i = 0; i < currentBoard.length; i++) {
-            // Checking if every cell in a row (currentBoard[i]) are the same as playerToken
-            const winByRow = currentBoard[i].every(
-                (cell) => cell.getValue() === playerToken,
-            );
+    for (let i = 0; i < currentBoard.length; i++) {
+      // Checking if every cell in a row (currentBoard[i]) are the same as playerToken
+      const winByRow = currentBoard[i].every(
+        (cell) => cell.getValue() === playerToken,
+      );
 
-            // Checking one token from specific row location(row[i]) compare it to other row's token of that same location
-            // to see if they're all the same as playerToken
-            const winByColumn = currentBoard.every(
-                (row) => row[i].getValue() === playerToken,
-            );
+      // Checking one token from specific row location(row[i]) compare it to other row's token of that same location
+      // to see if they're all the same as playerToken
+      const winByColumn = currentBoard.every(
+        (row) => row[i].getValue() === playerToken,
+      );
 
-            if (winByRow || winByColumn) {
-                winner = activePlayer;
-                console.log(`${activePlayer.name} WIN!`);
-                return true;
-            }
-        }
+      if (winByRow || winByColumn) {
+        winner = activePlayer;
+        gameOver = true;
+        console.log(`${activePlayer.name} WIN!`);
+        return true;
+      }
+    }
 
-        // Check main diagonal win from left to right
-        const mainDiagonalWin = currentBoard.every(
-            (row, index) => row[index].getValue() === playerToken,
-        );
+    // Check main diagonal win from left to right
+    const mainDiagonalWin = currentBoard.every(
+      (row, index) => row[index].getValue() === playerToken,
+    );
 
-        // Check anti diagonal win from right to left
-        const antiDiagonalWin = currentBoard.every(
-            (row, index) =>
-                row[row.length - index - 1].getValue() === playerToken,
-        );
+    // Check anti diagonal win from right to left
+    const antiDiagonalWin = currentBoard.every(
+      (row, index) => row[row.length - index - 1].getValue() === playerToken,
+    );
 
-        if (mainDiagonalWin || antiDiagonalWin) {
-            winner = activePlayer;
-            gameOver = true;
-            console.log(`${activePlayer.name} WIN!`);
-            return true;
-        }
+    if (mainDiagonalWin || antiDiagonalWin) {
+      winner = activePlayer;
+      gameOver = true;
+      console.log(`${activePlayer.name} WIN!`);
+      return true;
+    }
 
-        return false;
-    };
+    return false;
+  };
 
-    const checkTie = () => {
-        const currentBoard = board.getBoard();
-        const isFull = currentBoard.every((row) =>
-            row.every((cell) => cell.getValue() !== 0),
-        );
-        if (isFull && !winner) {
-            gameOver = true;
-            console.log("It's a Tie");
-            return true;
-        }
+  const checkTie = () => {
+    const currentBoard = board.getBoard();
+    const isFull = currentBoard.every((row) =>
+      row.every((cell) => cell.getValue() !== 0),
+    );
+    if (isFull && !winner) {
+      gameOver = true;
+      console.log("It's a Tie");
+      return true;
+    }
 
-        return false;
-    };
+    return false;
+  };
 
-    const playRound = (row, column) => {
-        if (gameOver) {
-            console.log("Game is over Please Reset the game!");
-            return;
-        }
+  const playRound = (row, column) => {
+    if (gameOver) {
+      console.log("Game is over Please Reset the game!");
+      return;
+    }
 
-        console.log(
-            `Adding ${getActivePlayer().name}'s Token into Cell ${row} ${column}...`,
-        );
+    console.log(
+      `Adding ${getActivePlayer().name}'s Token into Cell ${row} ${column}...`,
+    );
 
-        // Check if cell is available
-        if (board.getBoard()[row][column].getValue() !== 0) {
-            return console.log("Cell Already Taken!");
-        }
+    // Check if cell is available
+    if (board.getBoard()[row][column].getValue() !== 0) {
+      return console.log("Cell Already Taken!");
+    }
 
-        board.dropToken(row, column, getActivePlayer().token);
+    board.dropToken(row, column, getActivePlayer().token);
 
-        printNewRound();
-
-        if (checkWinner() || checkTie()) return;
-        switchPlayerTurn();
-        console.log(`${getActivePlayer().name}'s turn.`);
-    };
-
-    const getWinner = () => winner;
-
-    const isGameOver = () => gameOver;
-
-    const resetGame = () => {
-        board = GameBoard();
-        winner = null;
-        gameOver = null;
-        activePlayer = players[0];
-        printNewRound();
-        console.log("Game have been reset!");
-    };
-
-    // Initial play game message
     printNewRound();
 
-    return {
-        playRound,
-        getActivePlayer,
-        getWinner,
-        isGameOver,
-        getBoard: () => board.getBoard(),
-        resetGame,
-    };
+    if (checkWinner() || checkTie()) return;
+    switchPlayerTurn();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const getWinner = () => winner;
+
+  const isGameOver = () => gameOver;
+
+  const resetGame = () => {
+    board = GameBoard();
+    winner = null;
+    gameOver = null;
+    activePlayer = players[0];
+    printNewRound();
+    console.log("Game have been reset!");
+  };
+
+  // Initial play game message
+  printNewRound();
+
+  return {
+    playRound,
+    getActivePlayer,
+    getWinner,
+    isGameOver,
+    getBoard: () => board.getBoard(),
+    resetGame,
+  };
 }
 
 const ScreenController = () => {
-    const game = GameController();
-    const playerTurnDiv = document.querySelector(".turn");
-    const boardDiv = document.querySelector(".board");
-    const resultDiv = document.querySelector(".result");
-    const reset = document.querySelector(".game__action--retry");
+  const game = GameController();
+  const playerTurnDiv = document.querySelector(".turn");
+  const boardDiv = document.querySelector(".board");
+  const resultDiv = document.querySelector(".result");
+  const reset = document.querySelector(".game__action--retry");
 
-    const updateScreen = () => {
-        const activePlayer = game.getActivePlayer();
-        const winner = game.getWinner();
-        const board = game.getBoard();
+  const updateScreen = () => {
+    const activePlayer = game.getActivePlayer();
+    const winner = game.getWinner();
+    const board = game.getBoard();
 
-        // Remove Current Board
-        boardDiv.textContent = "";
+    // Remove Current Board
+    boardDiv.textContent = "";
 
-        boardDiv.style.setProperty(
-            "grid-template-columns",
-            `repeat(${board.length}, 1fr)`,
-        );
-        boardDiv.style.setProperty(
-            "grid-template-rows",
-            `repeat(${board.length}, 1fr)`,
-        );
+    boardDiv.style.setProperty(
+      "grid-template-columns",
+      `repeat(${board.length}, 1fr)`,
+    );
+    boardDiv.style.setProperty(
+      "grid-template-rows",
+      `repeat(${board.length}, 1fr)`,
+    );
 
-        // Print out who's turn it is
-        playerTurnDiv.textContent = `It's ${activePlayer.name}'s Turn.`;
+    // Print out who's turn it is
+    playerTurnDiv.textContent = `It's ${activePlayer.name}'s Turn.`;
 
-        // Print out current board
-        board.forEach((row, rowIndex) => {
-            row.forEach((cell, colIndex) => {
-                // Anything clickable should be a button!!
-                const cellButton = document.createElement("button");
-                const cellToken = document.createElement("p");
-                cellButton.classList.add("board__cell");
-                cellToken.classList.add("board__token");
+    // Print out current board
+    board.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        // Anything clickable should be a button!!
+        const cellButton = document.createElement("button");
+        const cellToken = document.createElement("p");
+        cellButton.classList.add("board__cell");
+        cellToken.classList.add("board__token");
 
-                // Create a data attribute to identify the column and row
-                // This makes it easier to pass into our `playRound` function
-                cellButton.dataset.row = rowIndex;
-                cellButton.dataset.column = colIndex;
+        // Create a data attribute to identify the column and row
+        // This makes it easier to pass into our `playRound` function
+        cellButton.dataset.row = rowIndex;
+        cellButton.dataset.column = colIndex;
 
-                cellToken.textContent =
-                    cell.getValue() === 0 ? "" : cell.getValue();
+        cellToken.textContent = cell.getValue() === 0 ? "" : cell.getValue();
 
-                if (cellToken.textContent === "O") {
-                    cellToken.style.setProperty(
-                        "color",
-                        "var(--color-playerOne)",
-                    );
-                } else if (cellToken.textContent === "X") {
-                    cellToken.style.setProperty(
-                        "color",
-                        "var(--color-playerTwo)",
-                    );
-                }
-
-                cellButton.appendChild(cellToken);
-                boardDiv.appendChild(cellButton);
-            });
-        });
-
-        if (winner) {
-            resultDiv.textContent = `${winner.name} is the WINNER!!!`;
-        } else if (game.isGameOver()) {
-            resultDiv.textContent = "It's a Tie!";
-        } else {
-            resultDiv.textContent = "";
+        if (cellToken.textContent === "O") {
+          cellToken.style.setProperty("color", "var(--color-playerOne)");
+        } else if (cellToken.textContent === "X") {
+          cellToken.style.setProperty("color", "var(--color-playerTwo)");
         }
-    };
 
-    // Listen for User Input then PlayRound
-    const handleClick = (e) => {
-        const btn = e.target.closest(".board__cell");
-        if (!btn) return;
+        cellButton.appendChild(cellToken);
+        boardDiv.appendChild(cellButton);
+      });
+    });
 
-        const { row, column } = btn.dataset;
-        game.playRound(row, column);
+    if (winner) {
+      resultDiv.textContent = `${winner.name} is the WINNER!!!`;
+    } else if (game.isGameOver()) {
+      resultDiv.textContent = "It's a Tie!";
+    } else {
+      resultDiv.textContent = "";
+    }
+  };
 
-        // Rerender screen after every user input
-        updateScreen();
-    };
+  // Listen for User Input then PlayRound
+  const handleClick = (e) => {
+    const btn = e.target.closest(".board__cell");
+    if (!btn) return;
 
-    const handleReset = () => {
-        game.resetGame();
-        updateScreen();
-    };
+    const { row, column } = btn.dataset;
+    game.playRound(row, column);
 
-    const init = () => {
-        boardDiv.addEventListener("click", handleClick);
-        reset.addEventListener("click", handleReset);
-        updateScreen();
-        console.log("Game Initialize!");
-    };
+    // Rerender screen after every user input
+    updateScreen();
+  };
 
-    return { init };
+  const handleReset = () => {
+    game.resetGame();
+    updateScreen();
+  };
+
+  const init = () => {
+    boardDiv.addEventListener("click", handleClick);
+    reset.addEventListener("click", handleReset);
+    updateScreen();
+    console.log("Game Initialize!");
+  };
+
+  return { init };
 };
 
 const game = ScreenController();
